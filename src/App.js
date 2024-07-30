9cee0453c6bed0aba6105c4ea8d036b6b0d2b88f
Reyyen cuneyt <rrayyan213@gmail.com>
2024-07-30 13:04:52 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
-
-function App() {
+import Header from './component/header';
+const name ="reyyen"
+const surname ="almasri"
+const issignIn = false
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //jsx yazarken kapsamli bir etikit kullamamiz lazim
+    //asagdaki div gibi yada fragment yani <>.....</> bos bir etiket
+    //class eklerken soyle ekleriz ork: <h1 className="">Hello reyyen</h1>, jsx ile js arasinda bir karisma olmVsin diye
+    // ayni durumda for icin gecerli <label htmlFor="myinput"></label>
+    <div>
+      <h2>{name} {surname}</h2>
+      <h1>Hello reyyen</h1>
+      {/* component isimini yazrken yada fonuctionu yazrken buyuk harfla yazmzmiz lazim */}
+      <Header />
+      {issignIn ?` name is: ${name}  surname is: ${surname}`: "giris yapadiniz"}
+      
     </div>
-  );
+  )
 }
-
-export default App;
+export default App; 
